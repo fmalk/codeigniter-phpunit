483e126c1c988891578f7fd6efe1d2cd11248981
Fery Wardiyanto <ferywardiyanto@gmail.com>
2014-07-10 04:14:25 +0700

    Autoloading all controllers due to fmalk/codeigniter-phpunit#8

--- application/tests/bootstrap.php
+++ application/tests/bootstrap.php
@@ -8,7 +8,7 @@
  * This will "override" later functions meant to be defined
  * in core\Common.php, so they throw erros instead of output strings
  */
- 
+
 function show_error($message, $status_code = 500, $heading = 'An Error Was Encountered')
 {
 	throw new PHPUnit_Framework_Exception($message, $status_code);
@@ -26,5 +26,10 @@ function show_404($page = '', $log_error = TRUE)
  *
  * Bootstrap CodeIgniter from index.php as usual
  */
- 
+
 require_once dirname(__FILE__) . '/../../index.php';
+
+foreach (glob(APPPATH.'controllers/**/*'.EXT) as $controller)
+{
+	require_once $controller;
+}
