7608299a1290be696fabb759fd5485949c42a403
Fery Wardiyanto <ferywardiyanto@gmail.com>
2015-02-09 20:05:54 +0700

    Workarounds according fmalk/codeigniter-phpunit#21: - [x] Add CI_VERSION enviroment on .travis.yml - [x] mv phpunit.xml application/test/phpunit.xml - [x] Add is_cli() function overwrite - [x] mv system/core system/core2 #for CI 2.x - [x] Add system/core3 #for CI 3.x

--- application/tests/bootstrap.php
+++ application/tests/bootstrap.php
@@ -19,6 +19,11 @@ function show_404($page = '', $log_error = TRUE)
 	throw new PHPUnit_Framework_Exception($page, 404);
 }
 
+function is_cli()
+{
+	return;
+}
+
 /*
  *---------------------------------------------------------------
  * BOOTSTRAP
@@ -31,7 +36,7 @@ require_once dirname(__FILE__) . '/../../index.php';
 
 /*
  * This will autoload controllers inside subfolders
- */ 
+ */
 spl_autoload_register(function ($class) {
 	foreach (glob(APPPATH.'controllers/**/'.strtolower($class).'.php') as $controller) {
 		require_once $controller;
